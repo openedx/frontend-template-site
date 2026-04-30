f000f06056559f6255a919b672b2cb7a9ff9397c
Adolfo R. Brandes <adolfo@axim.org>
2026-04-30 08:05:21 -0300

    feat: upgrade to brand-openedx ^1.3.0
    Now that brand-openedx 1.x has been updated to conform to the 2.x
    branding API - though still a noop - updated to it and bring back the
    brand imports in the site config files.
    
    Also update the package-lock for all @openedx dependencies.

--- site.config.build.tsx
+++ site.config.build.tsx
@@ -5,6 +5,8 @@ import { learnerDashboardApp } from '@openedx/frontend-app-learner-dashboard';
 import { notificationsApp } from '@openedx/frontend-app-notifications';
 
 import '@openedx/frontend-base/shell/style';
+import '@openedx/brand-openedx/core.min.css';
+import '@openedx/brand-openedx/light.min.css';
 
 const siteConfig: SiteConfig = {
   siteId: 'frontend-template-site',
